95b1fe0e66cd2cb1010e8b074d5e565cfd9d55aa
mcintyre321 <mcintyre321@gmail.com>
2012-06-26 13:23:42 +0200

    Update master

--- PiwikTrackerSharp/Properties/AssemblyInfo.cs
+++ PiwikTrackerSharp/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PiwikTrackerSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Call Piwik analytics from the server side")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("PiwikTrackerSharp")]
